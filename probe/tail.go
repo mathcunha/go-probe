2a03729c998bf11d1f4728b0accf8a7f2898bcee
Matheus Cunha <mathcunha@gmail.com>
2015-09-21 23:32:48 +0000

    refactoring tail

--- probe/tail.go
+++ probe/tail.go
@@ -4,11 +4,22 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
-	"time"
 )
 
-func ReadJVMMetrics() {
-	cmd := exec.Command("tail", "-n", "0", "-f", "/tmp/foo.txt")
+type TailCommand interface {
+	OutChannel() (out chan string)
+	QuitChannel() (quit chan bool)
+	Close()
+}
+
+type Tail struct {
+	ch   chan string
+	quit chan bool
+	cmd  *exec.Cmd
+}
+
+func Load(file string) TailCommand {
+	cmd := exec.Command("tail", "-n", "0", "-f", file)
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
@@ -16,37 +27,32 @@ func ReadJVMMetrics() {
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
 	}
-
-	ch := make(chan string)
-	quit := make(chan bool)
+	tail := &Tail{make(chan string), make(chan bool), cmd}
 	go func() {
 		buf := make([]byte, 1024)
 		for {
 			n, err := stdout.Read(buf)
 			if n != 0 {
-				ch <- string(buf[:n])
+				tail.ch <- string(buf[:n])
 			}
 			if err != nil {
 				break
 			}
 		}
 		fmt.Println("Goroutine finished")
-		close(ch)
+		close(tail.ch)
 	}()
+	return tail
+}
 
-	time.AfterFunc(time.Second, func() { quit <- true })
+func (t *Tail) QuitChannel() (quit chan bool) {
+	return t.quit
+}
 
-loop:
-	for {
-		select {
-		case s, ok := <-ch:
-			if !ok {
-				break loop
-			}
-			fmt.Print(s)
-		case <-quit:
-			cmd.Process.Kill()
-		}
-	}
-	fmt.Println("all done!")
+func (t *Tail) OutChannel() (out chan string) {
+	return t.ch
+}
+
+func (t *Tail) Close() {
+	t.cmd.Process.Kill()
 }
--- probe/tail_test.go
+++ probe/tail_test.go
@@ -1,9 +1,26 @@
 package probe
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
-func TestJVMMetrics(t *testing.T) {
-	ReadJVMMetrics()
+func TestLoad(t *testing.T) {
+	tail := Load("/tmp/jmvmetrics.go")
+	time.AfterFunc(time.Second, func() { tail.QuitChannel() <- true })
+
+loop:
+	for {
+		select {
+		case s, ok := <-tail.OutChannel():
+			if !ok {
+				break loop
+			}
+			fmt.Print(s)
+		case <-tail.QuitChannel():
+			tail.Close()
+		}
+	}
+	fmt.Println("all done!")
 }
