3936d797bc95e802b57eaa6d25f66b2052a0170f
Matheus Cunha <mathcunha@gmail.com>
2015-09-22 19:53:28 +0000

    each call to OutChannel generates a Go Routine

--- probe/tail_test.go
+++ probe/tail_test.go
@@ -10,11 +10,12 @@ func TestLoad(t *testing.T) {
 	tail := Load("/tmp/jmvmetrics.go")
 	quit := make(chan bool)
 	time.AfterFunc(4*time.Second, func() { quit <- true })
+	line := tail.OutChannel(true)
 
 loop:
 	for {
 		select {
-		case s, ok := <-tail.OutChannel(true):
+		case s, ok := <-line:
 			if !ok {
 				break loop
 			}
